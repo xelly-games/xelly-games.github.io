dcf23e986a0c2bfd77407fc45f7f4735c32a5c2f
Aaron Dixon <aarondixon@Grey-Goose.local>
2025-08-31 16:57:34 -0500

    Iter

--- content/well-knowns.ts
+++ content/well-knowns.ts
@@ -1,8 +1,8 @@
 export const urlGameGolfSolitaire =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:8083/embed/06f966d4-4729-4a66-af98-a01a38e6db72'
-        : 'https://xelly.games/game/7ade9820-f62d-484c-88e1-9af03130aead';
+        : 'https://xelly.games/embed/7ade9820-f62d-484c-88e1-9af03130aead';
 export const urlGameBreakout =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:8083/embed/440cd0c4-4eff-4467-9071-337bfed766c2'
-        : 'https://xelly.games/game/2043d642-a556-480e-be5a-8541f8fc147a';
+        : 'https://xelly.games/embed/2043d642-a556-480e-be5a-8541f8fc147a';
